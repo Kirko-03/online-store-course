64c1c50d543145634b2da0c11c2b93b63965c513
Kirito <kurulluvarov@gmail.com>
2022-02-06 15:08:36 +0300

    registration,login,check in userController,authMW

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,68 @@
-const apiError = require('../error/apiError')
-class UserController{
-   async registration(req,res){
+const apiError = require("../error/apiError");
+const bcrypt = require("bcrypt");
+const { User, Basket } = require("../models/model");
+const jwt = require('jsonwebtoken')
+function generateJWT(id,email,role){
+    return  jwt.sign(
+        {id,email,role},
+        process.env.SECRET_KEY,
+        {expiresIn:'24h'}
+        )
+}
+  //Функция генерации JWT токена
+class UserController {
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    //Вытягивание из тела запроса
+   
+    if (!email || !password) {
+      return next(
+        apiError.badRequest("Некорректный логин или пароль")
+      );
+    }
+    //Корректность данных
+    const candidate = await User.findOne({ where: { email } })
+    if (candidate) {
+      return next(
+        apiError.badRequest("Данный email зарегистрирован" )
+      );
+    }
+    //Обработка пользователя по email
+    const hashPassword = await bcrypt.hash(password, 5);
+    //Хеширование пароля
+    const user = await User.create({email,role,password:hashPassword})
+    //Создание пользователя
+    const basket = await Basket.create({userId:user.id})
+    //Создание корзины пользователя
+    const token = generateJWT(user.id,user.email,user.role) 
+    //Создание токена
+        return res.json({token})
+  }
 
+  //Метод регистрации пользователя
+  async login(req, res,next) {
+    const {email,password} = req.body
+    const user = await User.findOne({where:{email}})
+    if(!user){
+      return next(
+            apiError.internal('email не найден')
+        )
     }
-    async login(req,res){
+    let comparePassword = bcrypt.compareSync(password,user.password)
+    if(!comparePassword){
+        return next(
+            apiError.internal('Неправильный пароль')
+        )
+    }
+    const token = generateJWT(user.id,user.email,user.role) 
+    res.json({token})
 
-    }
-    async check(req,res,next){
-        const {id} = req.query
-        if(!id){
-           return next(apiError.badRequest('Не задан id'))
-        }
-        res.json(id)
-    }
+  }
+  //Метод логинизации пользователя
+  async check(req, res, next) {
+   const token = generateJWT(req.user.id,req.user.email,req.user.role)
+   return res.json({token})
+  }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express')
 const router = new Router() 
 const UserController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.get('/auth',UserController.check)
+
+router.get('/auth',authMiddleware,UserController.check)
 router.post('/login',UserController.login)
 router.post('/registration',UserController.registration)
 
