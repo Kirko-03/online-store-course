2a7a8217444ea343e000b0e5325d4cf1797ac00f
Kirito <kurulluvarov@gmail.com>
2022-02-06 11:31:43 +0300

    crud операции в controllers,раздача статики

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const sequelize = require("./db");
 const models = require("./models/model");
 const router = require("./routes/index")
 const cors = require("cors")
+const fileUpload = require("express-fileupload")
 const PORT = process.env.PORT || 5000;
 const app = express();
 const errorHandler = require('./middleware/errorHandlingMiddleware') 
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}))
+app.use(express.static(path.resolve(__dirname,'static')))
 app.use('/api',router)
 app.use(errorHandler)
 
